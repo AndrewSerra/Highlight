ffbbee9965c382cbe6928b2bce4acbd00a0c8839
Andrew Serra <andy@serra.us>
2019-06-18 16:34:20 +0300

    new note added as highlight button pressed, urls do not exceed the content box, highlight problematic

--- user_interfaces/js/popup.js
+++ user_interfaces/js/popup.js
@@ -5,41 +5,56 @@ chrome.storage.sync.get('storedItems', function(data) {
   console.log(data)
 
   data["storedItems"].forEach((elem) => {
-    let listItem = document.createElement("li")
-    let head = document.createElement("h3")
-    let span = document.createElement("span")
-
-    head.innerHTML = elem.note
-    span.innerHTML = elem.pageUrl
-
-    listItem.appendChild(head)
-    listItem.appendChild(span)
-
-    listItem.setAttribute("class", "list-item")
-
-    list.appendChild(listItem)
+    updateList(elem, list)
   })
 });
 
 addBtn.onclick = () => {
   const note = document.getElementById("highlight-note").value
-  let highlightSelection = window.getSelection()
+  let   highlightSelection = window.getSelection();
   let currentURL;
 
+  //alert(highlightSelection)
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    // console.log(tabs[0])
+    console.log(tabs[0])
     currentURL = tabs[0].url
   })
 
   chrome.storage.sync.get('storedItems', (data) => {
-    data["storedItems"].push({
+    const lastNote = {
       note: note,
       pageUrl: currentURL,
       highlight_text: highlightSelection
-    })
+    }
+
+    data["storedItems"].push(lastNote)
     console.log(data)
+
+    updateList(lastNote, list)
+
     chrome.storage.sync.set(data, (stored_data) => {
       console.log(`Data in storage: ${stored_data}`)
     })
   })
+}
+
+function updateList(data_obj, dom_parent) {
+
+  if(typeof data_obj !== 'object') {
+    throw "Function updateList has a parameter of type \"object\""
+  }
+
+  let listItem = document.createElement("li")
+  let head = document.createElement("h3")
+  let span = document.createElement("span")
+
+  head.innerHTML = data_obj.note
+  span.innerHTML = data_obj.pageUrl
+
+  listItem.appendChild(head)
+  listItem.appendChild(span)
+
+  listItem.setAttribute("class", "list-item")
+
+  list.appendChild(listItem)
 }
