cc9b98bf72146a783e7d7168d493763f60b1953c
Andrew Serra <andy@serra.us>
2019-06-18 23:07:55 +0300

    quick bug fix

--- user_interfaces/js/popup.js
+++ user_interfaces/js/popup.js
@@ -1,18 +1,22 @@
 let listContainer = document.getElementsByClassName('stored-text')[0]
-// let list = document.getElementsByClassName('stored-text-list')[0]
 let addBtn = document.getElementById('add-button')
 
 chrome.storage.sync.get('storedItems', function(data) {
   console.log(data)
 
+  // check if the array is empty
   if(data["storedItems"].length !== 0) {
+    // create dom elements for each object
     data["storedItems"].forEach((elem) => {
 
-      let list = document.createElement("ul").setAttribute("class", "stored-text-list");
+      let list = document.createElement("ul")
+      list.setAttribute("class", "stored-text-list");
       updateList(elem, list)
+      listContainer.appendChild(list)
     })
   }
   else {
+    // create message to indicate nothing is saved
     let noContentHeader = document.createElement("h1");
     noContentHeader.innerHTML = "Start Adding Notes!";
     setMultipleAttributes(noContentHeader, {
@@ -22,16 +26,19 @@ chrome.storage.sync.get('storedItems', function(data) {
   }
 });
 
+// highlight button click event
 addBtn.onclick = () => {
   const note = document.getElementById("highlight-note").value
   let   highlightSelection = window.getSelection();
   let currentURL;
 
+  // get the active tab
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     // console.log(tabs[0])
     currentURL = tabs[0].url
   })
 
+  // retrieve the already stored value
   chrome.storage.sync.get('storedItems', (data) => {
     const lastNote = {
       note: note,
@@ -46,15 +53,23 @@ addBtn.onclick = () => {
       noContentHeadMsg.style.display = "none";
     }
 
-
+    // add the last note
     data["storedItems"].push(lastNote)
     console.log(data)
 
     let list = document.getElementsByClassName('stored-text-list')[0]
+
+    // update the view
+    if(list === undefined) {
+      list = document.createElement("ul")
+      list.setAttribute("class", "stored-text-list");
+      listContainer.appendChild(list)
+    }
     updateList(lastNote, list)
 
+    // store the new object array
     chrome.storage.sync.set(data, (stored_data) => {
-      console.log(`Data in storage: ${stored_data}`)
+      console.log(stored_data)
     })
   })
 }
